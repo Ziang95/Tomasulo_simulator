75a0b39d5e8f082556456893d2400c1b827313a5
Randa <roe34@pitt.edu>
2019-10-30 19:32:27 -0400

    Created IQ and RF 32 and ROB
    if you found a problem tell me but it should be good to go

---
+++ InstBuffer.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class InstBuffer
+{
+public:
+	InstBuffer(){}
+    string Opcode;
+    string Dest;
+    string src1;
+    string src2;
+};
--- mips.cpp
+++ mips.cpp
@@ -1,11 +1,22 @@
 #include "mips.h"
+#include "config.h"
+#include "InstBuffer.h"
+#include "RegFiles.h"
+#include "ROB.h"
 
 using namespace std;
 
+config Config;
 clk_tick sys_clk = clk_tick(); //System clock
 memory main_mem = memory(MEM_LEN); //Main memory 
 mutex_t cout_lock = PTHREAD_MUTEX_INITIALIZER;
 mutex_t cerr_lock = PTHREAD_MUTEX_INITIALIZER;
+int IQ_entries; 			// No. of Instruction Buffer entries (should be calculated from the input parser).
+int ROB_Entries;
+RegFiles* RF; // pointer to 32 register files
+InstBuffer* IQ; // pointer to inst buffer
+ROB* Rob; //pointer to ROB
+
 
 clk_tick::clk_tick()
 {
@@ -98,6 +109,51 @@ void init_sys_clk() // Starts the primal VDD clock and wait until it goes stable
     }
     msg_log("sys_clk stablized", 0);
 }
+void initialize(){
+
+	//creat inst. buffer, Register file, ROB and intialize parameters needed
+
+Config.ReadInput();
+Config.SetVariables();
+    ROB_Entries = Config.getROB_LEN();
+	IQ_entries = (Config.instructions.size()); //no of inst we have
+
+	//creating ROB that has 128 entry(stated)---------------------------------------------------------------
+	Rob = new ROB[ROB_Entries];
+    //-----------------------------------------------------------------------------------------------------
+   // creating our instruction buffer
+
+	IQ = new InstBuffer[IQ_entries]; 
+
+	for (int i = 0; i < IQ_entries; i++) {
+		vector <string> temp = input.ReadInstruction(input.instructions[i]); //we know that temp size is always 4
+		(*(IQ + i)).Opcode = temp[0];
+		(*(IQ + i)).Dest = temp[1];
+		(*(Rob + i)).Dest = temp[1];
+		(*(IQ + i)).src1 = temp[2];
+		(*(IQ + i)).src2 = temp[3];
+}
+//creating the 32 Register file we have (fixed)
+
+	RF = new RegFiles[RegFiles_entries];
+	// every register is now hardwired to it's number R0 is at RF[0] ans so on
+
+	for (int i = 0; i < Config.register_var.size(); i++) { // my format is R1=10 , F2=30.1 ... etc will get which reg
+		string delimiter = "=";
+		string token = Config.register_var[i].substr(1, Config.register_var[i].find(delimiter) - 1); //token1 is 1, 2 to R1 and R2 ...etc
+		string value = Config.register_var[i].substr(Config.register_var[i].find(delimiter) + 1, Config.register_var[i].length());
+		int Reg_number = Config.convertStringToInt(token);
+		if (Config.register_var[i][0] == 'R') { //integer Register
+			RF[Reg_number].intRegFile = Config.convertStringToInt(value);
+		}
+		else { // float register
+			RF[Reg_number].floatRegFile = Config.convertStringToFloat(value);
+		}
+	}
+	//----------------------------------------------------------------------------------------------------------------
+	
+	
+}
 
 int main()
 {
@@ -111,4 +167,5 @@ int main()
     main_mem.enQ(LOAD, INTGR, 1, &li);
     main_mem.enQ(LOAD, FLTP, 2, &lf);
     cin.get();
+    initialize();
 }
